d2f57930cfaed4d624bd1fe9d79529b785417aac
ayeshoali <ayeshoali@gmail.com>
2022-07-14 19:03:12 +0500

    useMemo usage

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback,useMemo } from 'react';
 
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/Demo/DemoOutput';
 import './App.css';
+import DemoList from './components/Demo/DemoList';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
+  const [listTitle, setListTitle] = useState('My List');
 
-  console.log('APP RUNNING');
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, []);
 
-  const toggleParagraphHandler = useCallback(() => {
-    if (allowToggle) {
-      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-      //better way to update state
-      //in case of useEffect outstanding state changes are not missed
-    }
-  }, [allowToggle]);
-
-  const allowToggleHandler = () => {
-    setAllowToggle(true);
-  };
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
-      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
+// function App() {
+//   const [showParagraph, setShowParagraph] = useState(false);
+//   const [allowToggle, setAllowToggle] = useState(false);
+
+//   console.log('APP RUNNING');
+
+//   const toggleParagraphHandler = useCallback(() => {
+//     if (allowToggle) {
+//       setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+//       //better way to update state
+//       //in case of useEffect outstanding state changes are not missed
+//     }
+//   }, [allowToggle]);
+
+//   const allowToggleHandler = () => {
+//     setAllowToggle(true);
+//   };
+
+//   return (
+//     <div className="app">
+//       <h1>Hi there!</h1>
+//       <DemoOutput show={showParagraph} />
+//       <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+//       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+//     </div>
+//   );
+// }
 
 export default App;
 

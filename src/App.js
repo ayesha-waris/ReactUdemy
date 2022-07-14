a390e880be81a2686704008fc79e2b58370e710d
ayeshoali <ayeshoali@gmail.com>
2022-07-15 02:52:26 +0500

    wrapping fetch request in useEffect and useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React ,{useState} from 'react';
+import React ,{useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,35 +9,44 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-   const fetchMoviesHandler = async () => {
-     setIsLoading(true);
-     setError(null);
-    try{
-      const response = await fetch('https://swapi.dev/api/films');
-      
-      if (response.ok){
-        throw new Error('Error Occured');
-      }
+  const fetchMoviesHandler = useCallback( async () => {
+    setIsLoading(true);
+    setError(null);
+   try{
+     const response = await fetch('https://swapi.dev/api/films');
+     
+     if (!response.ok){
+       throw new Error('Error Occured');
+     }
 
-      const data = await response.json();
-    
-      const transformedData = data.results.map(movie => {
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          releaseDate: movie.release_date,
-          openingText:movie.opening_crawl
-        };
-      });
-      setMovies(transformedData) ;
-      setIsLoading(false);
-    } catch (error) {
-      setError(error.message);
-      
+     const data = await response.json();
+   
+     const transformedData = data.results.map(movie => {
+       return {
+         id: movie.episode_id,
+         title: movie.title,
+         releaseDate: movie.release_date,
+         openingText:movie.opening_crawl
+       };
+     });
+     setMovies(transformedData) ;
+     setIsLoading(false);
+   } catch (error) {
+     setError(error.message);
+     
 
-    }
-    setIsLoading(false);
-  }
+   }
+   setIsLoading(false);
+ }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+    return () => {
+      
+    };
+  }, [fetchMoviesHandler]);
+
+
   return (
     <React.Fragment>
       <section>

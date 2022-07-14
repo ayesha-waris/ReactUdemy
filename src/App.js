444e09cade1d0790d7ca33f09baf459c139f8c2e
ayeshoali <ayeshoali@gmail.com>
2022-07-14 18:47:09 +0500

    STATE SCHEDULING AND BATCHING NOTES

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
   const toggleParagraphHandler = useCallback(() => {
     if (allowToggle) {
       setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+      //better way to update state
+      //in case of useEffect outstanding state changes are not missed
     }
   }, [allowToggle]);
 
@@ -32,12 +34,26 @@ function App() {
 
 export default App;
 
+//React.memo()
 //when parent component re-evaluatesall its child components also even if props value do not change
 // React.memo() is used to resolve this issue, it stores previous props value and compare it to the ne props
 // it costs performance as well for larger projects
 //useful for long tree to avoid re-evaluation
 
+//useCallback()
 // for objects useMemo does not not work
 // copies of objects are made which are of reference type and they're not same
 //useCallback is used to resolve this issue, it allows a function to save it exactly and use same object wwhen component re-evaluates.
 // dependencies are used to recreate function because values of function coming from from outside are changed,  to use latest value in function
+
+
+
+//useState is handled by react itself, when component re-evaluates no new states is made
+//state is only changed after firstt initialisation
+//unless component is removed
+
+//state update is scheduled when component reruns
+//STATE BATCHING
+//react batches states updates together for synchronuous functions(start- end)
+
+

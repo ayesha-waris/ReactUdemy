f804e72bd6fc497619924a83c397e13dc795c4fa
ayeshoali <ayeshoali@gmail.com>
2022-07-14 22:21:11 +0500

    component life cycle with context

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,46 +1,40 @@
-import { Fragment, useState, useEffect ,Component} from 'react';
+import { Fragment, useState, useEffect, Component } from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
+import UsersContext from '../store/users-context';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
 class UserFinder extends Component {
-  constructor(){
+  static contextType = UsersContext;
+
+  constructor() {
     super();
     this.state = {
-      filteredUsers: DUMMY_USERS,
-      searchTerm: ''
+      filteredUsers: [],
+      searchTerm: '',
     };
-    
-  };
-  componentWillUnmount () {
-    console.log("unmounted");
   }
+
   componentDidMount() {
-// send http request
-//it will run only once
+  // Send http request...
+  //it will run only once
 //useEffect with no dependencies
-  };
-
-componentDidUpdate(prevProps, prevState) {
-  if(prevState.searchTerm !== this.state.searchTerm){
-      this.setState({
-        filteredUsers: DUMMY_USERS.filter( (user) => 
-        user.name.includes(this.state.searchTerm))
-    
-      });
+    this.setState({ filteredUsers: this.context.users });
   }
-};
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
 
-  searchChangeHandler (event) {
-    this.setState ({searchTerm: event.target.value});
-
-  };
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
 
   render() {
     return (
@@ -53,6 +47,7 @@ componentDidUpdate(prevProps, prevState) {
     );
   }
 }
+
 // const UserFinder = () => {
 //   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
 //   const [searchTerm, setSearchTerm] = useState('');

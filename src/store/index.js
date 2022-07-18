80bf90023f3b81c28faa2de1eebf5172cd64eeca
ayeshoali <ayeshoali@gmail.com>
2022-07-18 17:51:46 +0500

    redux toolkit added, changed reducers to createSlice and used configStore for store formation

--- src/store/index.js
+++ src/store/index.js
@@ -1,41 +1,34 @@
 
-import {createStore} from 'redux';
+
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialStates = {counter: 0, showCounter: true};
-const counterReducer = (state= initialStates, action) => {
-
-  if(action.type === 'increment'){
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter
-    };
+const counterSlice = createSlice({
+  name:'counter',
+  initialStates,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement (state) {
+      state.counter--;
+    },
+    increase (state, action) {
+      state.counter = state.counter + action.value;
+    },
+    toggle (state) {
+      state.showCounter = !state.showCounter;
+    }
   }
-   if(action.type === 'increase'){
-    return {
-      counter: state.counter + action.value ,
-      showCounter: state.showCounter
-    };
-  }
-  
-   if(action.type === 'decrement'){
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter
-    };
-  }
+});
 
-  if(action.type === 'toggle'){
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter
-   
-    };
-  }
 
-  return state;
-};
 
-const store = createStore(counterReducer);
+const store = configureStore({
+  //for single stateSlice 
+  reducer: counterSlice.reducer 
+  //for multiple stateSlice use object (key value pairs)
+});
 
 
 export default store;

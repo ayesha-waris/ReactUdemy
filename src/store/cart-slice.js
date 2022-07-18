7b06ebc572e531b74b693bbb783ca988985e89c3
ayeshoali <ayeshoali@gmail.com>
2022-07-19 01:42:23 +0500

    redux with action creator function

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { uiActions } from './ui-slice';
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -40,6 +41,45 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCardData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!',
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        'https://httprequest-34399-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+    };
+    try {   
+       await sendRequest();
+      }catch(error) {
+        dispatch(
+          uiActions.showNotification({
+            status: 'success',
+            title: 'Success!',
+            message: 'Sent cart data successfully!',
+          })
+        );
+      }
+
+    
+  };
+};
 
 export const cartActions = cartSlice.actions;
 

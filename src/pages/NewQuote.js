e18344da7c9ba44939db58551cd7e8a38dfbbe4d
ayeshoali <ayeshoali@gmail.com>
2022-07-20 18:10:23 +0500

    programattic navigation added

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,12 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
   const onAddQuote = (quote) => {
     console.log(quote);
+    history.push('/quotes');
+    history.replace('/quotes');
   }
   return <QuoteForm onAddQuote = {onAddQuote}/>
 };
